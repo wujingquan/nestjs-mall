07837994caf8d9d3ba435fda87b1ef636257fcd8
wujingquan <i@wujingquan.com>
2020-01-15 19:30:29 +0800

    权限添加功能

--- src/module/admin/access/access.controller.ts
+++ src/module/admin/access/access.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Render, Query } from '@nestjs/common';
+import { Controller, Get, Render, Query, Body, Post, Response } from '@nestjs/common';
 import { Config } from 'src/config/index';
 import { ToolsService } from '../../../services/tools/tools.service';
 import { AccessService } from '../../../services/access/access.service';
+import * as mongoose from 'mongoose';
 
 @Controller(`${Config.adminPath}/access`)
 export class AccessController {
@@ -42,8 +43,51 @@ export class AccessController {
     })
     const accessResult = await this.accessService.find({ _id: query.id })
     return {
-      list:accessResult[0],
-      moduleList:result
-  };
+      list: accessResult[0],
+      moduleList: result
+    };
+  }
+
+  @Post('doEdit')
+  async doEdit(@Body() body, @Response() res) {
+    const {
+      module_id,
+      _id
+    } = body
+
+    try {
+      if (module_id != 0) {
+        body.module_id = mongoose.Types.ObjectId(module_id);
+      }
+
+      await this.accessService.update({
+        _id
+      }, body)
+      this.toolsService.success(res, `/${Config.adminPath}/access`);
+    } catch (err) {
+      this.toolsService.error(res, '非法请求', `/${Config.adminPath}/access/edit?id=${_id}`);
+    }
+  }
+
+  @Get('add')
+  @Render('admin/access/add')
+  async add() {
+    const result = await this.accessService.find({ module_id: '0' })
+    return {
+      moduleList: result
+    }
+  }
+
+  @Post('doAdd')
+  async doAdd(@Body() body, @Response() res) {
+    const {
+      module_id
+    } = body
+    if (module_id != 0) {
+      body.module_id = mongoose.Types.ObjectId(module_id);
+    }
+
+    await this.accessService.add(body)
+    this.toolsService.success(res, `/${Config.adminPath}/access`);
   }
 }
--- src/services/access/access.service.ts
+++ src/services/access/access.service.ts
@@ -6,11 +6,29 @@ export class AccessService {
   constructor(
     @InjectModel('Access')
     private readonly accessModel,
-  ) {}
+  ) { }
 
   async find(json) {
     try {
       return await this.accessModel.find(json);
+    } catch (err) {
+      return null;
+    }
+  }
+
+  async update(json1, jons2) {
+    try {
+      return await this.accessModel.updateOne(json1, jons2)
+    } catch (err) {
+      return null;
+    }
+  }
+
+  async add(json) {
+    try {
+      const access = new this.accessModel(json)
+      const result = await access.save()
+      return result
     } catch (err) {
       return null;
     }
